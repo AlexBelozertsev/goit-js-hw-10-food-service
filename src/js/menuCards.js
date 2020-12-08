e1d49914cc8b2ccebfb843fed9cc9b7255ffecb0
AlexBelozertsev <kashim983@gmail.com>
2020-12-09 01:10:02 +0200

    add filter

--- src/js/menuCards.js
+++ src/js/menuCards.js
@@ -1,27 +1,46 @@
 import menu from '../menu.json';
 import menuCardsTpl from '../templates/menuCards.hbs';
+import filterTpl from '../templates/filter.hbs';
 
 const menuCardList = document.querySelector('.js-menu');
+const filterBar = document.querySelector('.filter-bar');
 
-menuCardList.insertAdjacentHTML('beforeend', createMenuCardsMarkup(menu));
+filterBar.insertAdjacentHTML('beforeend', createfilterMarkup(menu));
+filterBar.addEventListener('click', onButtonClick);
+createMarkup(menu);
 
+function createfilterMarkup(menu) {
+    const arr = menu
+    .reduce((acc, value) => [...acc, ...value.ingredients], [])
+    .filter((v, i, a) => a.indexOf(v) === i)
+    .sort();
+    return filterTpl(arr);
+};
 function createMenuCardsMarkup(menu) {
     return menuCardsTpl(menu);
 };
 
-let result = [];
-menuCardList.addEventListener('click', onIngridientClick);
-
-function onIngridientClick(e) {
+function createMarkup(el) {
+    menuCardList.insertAdjacentHTML('beforeend', createMenuCardsMarkup(el))
+};
+function onButtonClick(e) {
     if (!e.target.classList.contains('tag-list__item')) {
         return;
     }
+    let result = [];
     const value = e.target.textContent;
-    menu.map(item => {
-        if (item.ingredients.includes(value)) {
-            result.push(item);
-        }
-    });
-    menuCardList.innerHTML = '';
-    menuCardList.insertAdjacentHTML('beforeend', createMenuCardsMarkup(result));
-}
+    if (value === 'Все') {
+        menuCardList.innerHTML = '';
+        createMarkup(menu);
+    } else {
+            menu.map(item => {
+            if (item.ingredients.includes(value)) {
+                result.push(item);
+            }
+        });
+        menuCardList.innerHTML = '';
+        createMarkup(result);
+        result = [];
+    }
+    
+};
