667f599aa34fe1f1713d1f5a6c057dfb5075c2bf
AlexBelozertsev <kashim983@gmail.com>
2020-12-12 22:25:49 +0200

    корректировка

--- src/js/menuCards.js
+++ src/js/menuCards.js
@@ -10,26 +10,25 @@ filterBar.addEventListener('click', onButtonClick);
 createMarkup(menu);
 
 function createfilterMarkup(menu) {
-    const arr = menu
-    .reduce((acc, value) => [...acc, ...value.ingredients], [])
-    .filter((v, i, a) => a.indexOf(v) === i)
-    .sort();
-    return filterTpl(arr);
-};
-function createMenuCardsMarkup(menu) {
-    return menuCardsTpl(menu);
-};
-
-function createMarkup(el) {
-    menuCardList.insertAdjacentHTML('beforeend', createMenuCardsMarkup(el))
-};
+        const arr = menu
+        .reduce((acc, value) => [...acc, ...value.ingredients], [])
+        .filter((val, idx, arr) => arr.indexOf(val) === idx)
+        .sort();
+        return filterTpl(arr);
+    };
+    function createMenuCardsMarkup(menu) {
+        return menuCardsTpl(menu);
+    };
+    function createMarkup(el) {
+        menuCardList.insertAdjacentHTML('beforeend', createMenuCardsMarkup(el))
+    };
 function onButtonClick(e) {
     if (!e.target.classList.contains('tag-list__item')) {
         return;
     }
     let result = [];
     const value = e.target.textContent;
-    if (value === 'Все') {
+    if (e.target.classList.contains('all')) {
         menuCardList.innerHTML = '';
         createMarkup(menu);
     } else {
@@ -42,5 +41,4 @@ function onButtonClick(e) {
         createMarkup(result);
         result = [];
     }
-    
 };
